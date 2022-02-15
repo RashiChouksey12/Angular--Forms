a049b11129fd26a49e75d293023cf5704bd4f652
rashi12 <rashi.chouksey@successive.tech>
2022-02-15 20:08:48 +0530

    feature/Task-2: code updated

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,15 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { ReactiveFormComponent } from './components/reactive-form/reactive-form.component';
-import { TampleteComponent } from './components/tamplete/tamplete.component';
+import { ReactiveFormComponent } from './Components/reactive-form/reactive-form.component';
 import { TamplateFormComponent } from './Components/tamplate-form/tamplate-form.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ReactiveFormComponent,
-    TampleteComponent,
-    TamplateFormComponent
+    TamplateFormComponent,
+    ReactiveFormComponent
+    
   ],
   imports: [
     BrowserModule,
