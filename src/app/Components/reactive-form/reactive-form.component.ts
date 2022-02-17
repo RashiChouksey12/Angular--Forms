38145809253b79f158fd12a57020470e160bec95
rashi12 <rashi.chouksey@successive.tech>
2022-02-17 12:38:23 +0530

    feature/Task-2: code updated

--- src/app/Components/reactive-form/reactive-form.component.ts
+++ src/app/Components/reactive-form/reactive-form.component.ts
@@ -1,15 +1,28 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl,FormGroup,FormBuilder} from '@angular/forms';
 
 @Component({
   selector: 'app-reactive-form',
   templateUrl: './reactive-form.component.html',
   styleUrls: ['./reactive-form.component.css']
 })
-export class ReactiveFormComponent implements OnInit {
+export class ReactiveFormComponent {
+  RegisterForm:FormGroup;
 
-  constructor() { }
+  constructor(private fb: FormBuilder){}
 
-  ngOnInit(): void {
+  ngOnInit():void {
+
+    this.RegisterForm= this.fb.group({
+      fullName :[''],
+      userName:[''],
+      email :[''],
+      password :[''],
+      confirmPassword :[''],
+      checkBox:[''],
+    }); 
   }
-
+  onSubmit() {
+    console.log(this.RegisterForm.value);
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,9 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ReactiveFormsModule,FormsModule } from '@angular/forms';
+
 import { ReactiveFormComponent } from './Components/reactive-form/reactive-form.component';
 import { TamplateFormComponent } from './Components/tamplate-form/tamplate-form.component';
 
@@ -15,7 +16,10 @@ import { TamplateFormComponent } from './Components/tamplate-form/tamplate-form.
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ReactiveFormsModule,
+    FormsModule,
+  
   ],
   providers: [],
   bootstrap: [AppComponent]
