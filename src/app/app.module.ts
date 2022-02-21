26c0267f0b2323d7ee0cc2d5e76e61b2a010b699
rashi12 <rashi.chouksey@successive.tech>
2022-02-21 12:53:46 +0530

    feature/Task-3: task completed

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,8 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ReactiveFormsModule,FormsModule } from '@angular/forms';
-import { RouterModule } from '@angular/router';import { ReactiveFormComponent } from './Components/reactive-form/reactive-form.component';
+import { RouterModule } from '@angular/router';
+import { ReactiveFormComponent } from './Components/reactive-form/reactive-form.component';
 import { TamplateFormComponent } from './Components/tamplate-form/tamplate-form.component';
 
 @NgModule({
@@ -19,7 +20,7 @@ import { TamplateFormComponent } from './Components/tamplate-form/tamplate-form.
     ReactiveFormsModule,
     FormsModule,
     RouterModule.forRoot([
-      {path: 'reactive-form', component: ReactiveFormComponent},
+      {path: '', component: ReactiveFormComponent},
       {path: 'tamplate-form', component: TamplateFormComponent},
       ]),
    
